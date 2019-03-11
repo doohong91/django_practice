5c73db5ad9626cdbcb5ce906161a69dc024c7fa3
doohong91 <doohong91@gmail.com>
2019-03-11 12:00:00 +0900

    migrate model

---
+++ board/views.py
@@ -0,0 +1,29 @@
+from django.shortcuts import render
+
+# Create your views here.
+def article_list(request):
+    return render(request, 'board/index.html')
+
+
+def article_detail(request, article_id):
+    pass
+
+
+def new_article(request):
+    pass
+
+
+def create_article(request):
+    pass
+
+
+def edit_article(request, article_id):
+    pass
+
+
+def update_article(request, article_id):
+    pass
+
+
+def delete_article(request, article_id):
+    pass
